69f84e709e3641ce4c596dc36d6a9d8dd972cce9
Mateusz Czeladka <mateusz.czeladka@cardanofoundation.org>
2023-07-11 15:09:51 +0200

    Fix: votingPower is in lovelaces

--- backend-services/voting-app/src/main/java/org/cardano/foundation/voting/service/voting_power/VotingPowerService.java
+++ backend-services/voting-app/src/main/java/org/cardano/foundation/voting/service/voting_power/VotingPowerService.java
@@ -5,10 +5,7 @@ import org.cardano.foundation.voting.service.blockchain_state.BlockchainDataStak
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.math.BigInteger;
 import java.util.Optional;
-
-import static com.bloxbean.cardano.client.common.ADAConversionUtil.lovelaceToAda;
 
 @Service
 public class VotingPowerService {
@@ -20,24 +17,10 @@ public class VotingPowerService {
         return switch (event.getVotingEventType()) {
             case USER_BASED -> throw new RuntimeException("voting power for USER_BASED events is not supported!");
             case STAKE_BASED -> {
-                yield blockchainDataStakePoolService.getStakeAmount(event.getSnapshotEpoch(), stakeAddress)
-                        .map(stakeAmount -> {
-                            if (stakeAmount < 1_000_000L) {
-                                return 0L;
-                            }
-
-                            return lovelaceToAda(BigInteger.valueOf(stakeAmount)).longValue();
-                        });
+                yield blockchainDataStakePoolService.getStakeAmount(event.getSnapshotEpoch(), stakeAddress);
             }
             case BALANCE_BASED -> {
-                yield blockchainDataStakePoolService.getBalanceAmount(event.getSnapshotEpoch(), stakeAddress)
-                        .map(stakeAmount -> {
-                            if (stakeAmount < 1_000_000L) {
-                                return 0L;
-                            }
-
-                            return lovelaceToAda(BigInteger.valueOf(stakeAmount)).longValue();
-                        });
+                yield blockchainDataStakePoolService.getBalanceAmount(event.getSnapshotEpoch(), stakeAddress);
             }
         };
     }

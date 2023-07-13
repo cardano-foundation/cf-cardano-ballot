1f1f8db07b643f301c4f6aab9743fbcfe5b40e25
Vitalii Vozniak <vetalcore@gmail.com>
2023-07-13 12:01:35 +0300

    chore(cip-16941): rtk example

--- ui/cip-1694/src/common/utils/sessionUtils.ts
+++ ui/cip-1694/src/common/utils/sessionUtils.ts
@@ -1,6 +1,6 @@
 import { USER_SESSION_KEY } from '../constants/localConstants';
-import { store } from '../redux/configureStore';
-import { CLEAR_SESSION } from '../redux/actionTypes';
+// import { store } from '../store';
+// import { CLEAR_SESSION } from '../store/actionTypes';
 
 export const saveUserInSession = (session: any) =>
   sessionStorage.setItem(USER_SESSION_KEY, JSON.stringify(session));
@@ -13,7 +13,8 @@ export const loadUserInSession = () => {
 export const clearUserInSessionStorage = () => {
   sessionStorage.removeItem(USER_SESSION_KEY);
   sessionStorage.clear();
-  store.dispatch({
-    type: CLEAR_SESSION,
-  });
+  // store.dispatch({
+  //   type: CLEAR_SESSION,
+  // });
+  // do const dispatch = useDispatch(); dispatch(clearSession()) on components level instead
 }

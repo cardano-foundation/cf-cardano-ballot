d118b4029ab4b01d757dfa95a2a2e178f0866e31
Mateusz Czeladka <mateusz.czeladka@cardanofoundation.org>
2023-08-23 15:01:10 +0200

    Fix: small bug fixes

--- backend-services/voting-ledger-follower-app/src/main/java/org/cardano/foundation/voting/resource/BlockchainDataResource.java
+++ backend-services/voting-ledger-follower-app/src/main/java/org/cardano/foundation/voting/resource/BlockchainDataResource.java
@@ -6,6 +6,7 @@ import org.cardano.foundation.voting.service.blockchain_state.BlockchainDataChai
 import org.cardano.foundation.voting.service.blockchain_state.BlockchainDataTransactionDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,13 +33,12 @@ public class BlockchainDataResource {
                         chainTip -> ResponseEntity.ok().body(chainTip));
     }
 
-    @RequestMapping(value = "/tx-details", method = GET, produces = "application/json")
+    @RequestMapping(value = "/tx-details/{txHash}", method = GET, produces = "application/json")
     @Timed(value = "resource.tx-details", percentiles = { 0.3, 0.5, 0.95 })
-    public ResponseEntity<?> txDetails(String txHash) {
+    public ResponseEntity<?> txDetails(@PathVariable String txHash) {
         return blockchainDataTransactionDetailsService.getTransactionDetails(txHash)
                 .map(txDetails -> ResponseEntity.ok().body(txDetails))
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
 }
-

738e2f6809ac3974f3317a90bf5206e95ca6c649
Giovanni Gargiulo <giovanni.gargiulo@cardanofoundation.org>
2023-08-24 13:28:12 +0100

    chore: hardcoding backend

--- ui/cip-1694/src/setupProxy.js
+++ ui/cip-1694/src/setupProxy.js
@@ -1,31 +1,33 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+
+
 module.exports = function (app) {
   app.use(
     '/api/verification/verify-vote',
     createProxyMiddleware({
-      target: 'http://localhost:9092',
+      target: 'https://verification-api.dev.cf-cip1694-preprod.eu-west-1.metadata.dev.cf-deployments.org',
       changeOrigin: true,
     })
   );
   app.use(
     '/api/vote/**',
     createProxyMiddleware({
-      target: 'http://localhost:9091',
+      target: 'https://api.dev.cf-cip1694-preprod.eu-west-1.metadata.dev.cf-deployments.org',
       changeOrigin: true,
     })
   );
   app.use(
     '/api/leaderboard/**',
     createProxyMiddleware({
-      target: 'http://localhost:9090',
+      target: 'https://follower-api.dev.cf-cip1694-preprod.eu-west-1.metadata.dev.cf-deployments.org',
       changeOrigin: true,
     })
   );
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:9090',
+      target: 'https://follower-api.dev.cf-cip1694-preprod.eu-west-1.metadata.dev.cf-deployments.org',
       changeOrigin: true,
     })
   );

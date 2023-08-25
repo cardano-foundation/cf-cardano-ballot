048c0933cba972cfa69a80f3553d810f7f35f512
Mateusz Czeladka <mateusz.czeladka@cardanofoundation.org>
2023-08-25 14:54:50 +0200

    Feature: implemented ChainSyncService as well as chain sync indicator.

---
+++ backend-services/voting-ledger-follower-app/src/main/java/org/cardano/foundation/voting/service/chain_sync/ChainSyncService.java
@@ -0,0 +1,77 @@
+package org.cardano.foundation.voting.service.chain_sync;
+
+import com.bloxbean.cardano.client.api.exception.ApiException;
+import com.bloxbean.cardano.client.backend.blockfrost.service.BFBackendService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Slf4j
+public class ChainSyncService {
+
+    @Autowired
+    @Qualifier("original_blockfrost")
+    private BFBackendService orgBackendService;
+
+    @Autowired
+    @Qualifier("yaci_blockfrost")
+    private BFBackendService yaciBackendService;
+
+    @Value("${chain.sync.buffer:5}")
+    private final int chainSyncBuffer = 5;
+
+    public SyncStatus getSyncStatus() {
+        try {
+            var orgLastBlockResult = orgBackendService.getBlockService().getLatestBlock();
+            var yaciLastBlockResult = yaciBackendService.getBlockService().getLatestBlock();
+
+            if (orgLastBlockResult.isSuccessful() && yaciLastBlockResult.isSuccessful()) {
+                long diff = orgLastBlockResult.getValue().getSlot() - yaciLastBlockResult.getValue().getSlot();
+
+                log.info("Current diff: {} (slots) between org and yaci.", diff);
+
+                boolean isSynced = diff >= 0 && diff <= chainSyncBuffer;
+
+                if (isSynced) {
+                    return SyncStatus.ok(diff);
+                }
+
+                log.warn("Yaci is not synced with the original chain. Diff: {} (slots)", diff);
+
+                return SyncStatus.notYet(diff);
+            }
+
+            return SyncStatus.unknownError();
+        } catch (ApiException e) {
+            log.error("Backend service is not available: {}", e.getMessage());
+
+            return SyncStatus.error(e);
+        }
+    }
+
+    public record SyncStatus(boolean isSynced, Optional<Long> diff, Optional<Exception> ex) {
+
+        static SyncStatus ok(long diff) {
+            return new SyncStatus(true, Optional.of(diff), Optional.empty());
+        }
+
+        static SyncStatus notYet(long diff) {
+            return new SyncStatus(false, Optional.of(diff), Optional.empty());
+        }
+
+        static SyncStatus error(Exception ex) {
+            return new SyncStatus(false, Optional.empty(), Optional.of(ex));
+        }
+
+        static SyncStatus unknownError() {
+            return new SyncStatus(false, Optional.empty(), Optional.empty());
+        }
+
+    }
+
+}
--- backend-services/voting-ledger-follower-app/src/main/java/org/cardano/foundation/voting/service/health/YaciStoreTipHealthIndicator.java
+++ backend-services/voting-ledger-follower-app/src/main/java/org/cardano/foundation/voting/service/health/YaciStoreTipHealthIndicator.java
@@ -1,21 +1,44 @@
 package org.cardano.foundation.voting.service.health;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.cardano.foundation.voting.service.chain_sync.ChainSyncService;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("yaci_store_chain_sync")
 @Slf4j
+@RequiredArgsConstructor
 public class YaciStoreTipHealthIndicator implements HealthIndicator {
+
+    private final ChainSyncService chainSyncService;
 
     @Override
     public Health health() {
-        // TODO Yaci-Store integration to check if we have joined the tip
+        var syncStatus = chainSyncService.getSyncStatus();
+
+        if (syncStatus.isSynced()) {
+            return Health
+                    .up()
+                    .withDetail("message", "Yaci-Store synced with the original chain!")
+                    .withDetail("diffSlots", syncStatus.diff().orElse(-1L))
+                    .build();
+        }
+
+        if (syncStatus.ex().isPresent()) {
+            return Health
+                    .down()
+                    .withDetail("message", "Yaci-Store error...")
+                    .withDetail("diffSlots", syncStatus.diff().orElse(-1L))
+                    .withException(syncStatus.ex().get())
+                    .build();
+        }
 
         return Health
-                .up()
-                .withDetail("message", "Yaci-Store integration not implemented yet!")
+                .down()
+                .withDetail("message", "Yaci-Store is syncing...")
+                .withDetail("diffSlots", syncStatus.diff().orElse(-1L))
                 .build();
     }
 

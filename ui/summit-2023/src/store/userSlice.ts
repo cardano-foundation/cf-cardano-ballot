ade2a2447c16bbe6bcd7489548c16e1d6a3f3240
Satya Muntha <satya.muntha@cardanofoundation.org>
2023-09-06 12:15:38 +0100

    fix: removing unavailable presentation namefrom reducer

--- ui/summit-2023/src/store/userSlice.ts
+++ ui/summit-2023/src/store/userSlice.ts
@@ -14,7 +14,6 @@ const initialState: UserState = {
   event: {
     id: '',
     team: '',
-    presentationName: '',
     votingEventType: 'USER_BASED',
     startSlot: undefined,
     endSlot: undefined,

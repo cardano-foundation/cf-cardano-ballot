2d7be2d140cdca9728c8e52a9399c1929e09ce73
Satya Muntha <satya.muntha@cardanofoundation.org>
2023-09-08 14:02:26 +0100

    fix: fix layout

--- ui/summit-2023/src/routes/index.tsx
+++ ui/summit-2023/src/routes/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { Nominees } from '../pages/Nominees';
-import { Leaderboard } from 'pages/Leaderboard';
+import { Leaderboard } from '../pages/Leaderboard';
 import { NotFound } from '../pages/NotFound';
 import { Categories } from '../pages/Categories';
 

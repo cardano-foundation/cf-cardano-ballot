de54ee3cd22f247bd1fb8358aa3ae3d36e2d1504
Vitalii Vozniak <vetalcore@gmail.com>
2023-09-19 21:58:20 +0300

    fix(cip-1694-ui): optimize event image size, hide discrod icon

--- ui/cip-1694/src/pages/Vote/Vote.tsx
+++ ui/cip-1694/src/pages/Vote/Vote.tsx
@@ -264,10 +264,11 @@ export const VotePage = () => {
         console.log('Failed to cast e vote', error?.message || error.toString());
       }
     }
-    setIsCastingAVote(true);
+    setIsCastingAVote(false);
   };
 
   const onChangeCategory = () => {
+    setVoteSubmitted(false);
     setIsReceiptFetched(false);
     const currentCategoryIndex = findIndex(event?.categories, ['id', category]);
     setCategory(event?.categories[(currentCategoryIndex + 1) % event?.categories?.length]?.id);

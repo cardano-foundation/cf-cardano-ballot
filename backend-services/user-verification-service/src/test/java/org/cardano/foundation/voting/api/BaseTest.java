d77d1045d47cf70b664a33e5391de89c52ce9b92
Fabian Bormann <fabian.bormann@cardanofoundation.org>
2023-09-30 21:28:55 +0200

    chore: cleanup base test

--- backend-services/user-verification-service/src/test/java/org/cardano/foundation/voting/api/BaseTest.java
+++ backend-services/user-verification-service/src/test/java/org/cardano/foundation/voting/api/BaseTest.java
@@ -3,25 +3,13 @@ package org.cardano.foundation.voting.api;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import io.restassured.RestAssured;
-
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
-import org.testcontainers.containers.localstack.LocalStackContainer;
-import org.testcontainers.utility.DockerImageName;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.sns.SnsClient;
-
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
-import static org.testcontainers.containers.localstack.LocalStackContainer.Service.SNS;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ActiveProfiles("test")
@@ -29,14 +17,6 @@ public class BaseTest {
 
     @LocalServerPort
     private int serverPort;
-    @Value("${aws.sns.accessKeyId}")
-    private String awsAccessKeyId;
-    @Value("${aws.sns.secretAccessKey}")
-    private String awsSecretAccessKey;
-    @Value("${aws.sns.region}")
-    private String awsRegion;
-    private LocalStackContainer localstackContainer;
-    private SnsClient snsClient;
     private WireMockServer wireMockServer;
 
     @BeforeAll
@@ -52,33 +32,7 @@ public class BaseTest {
                                 .withHeader("Content-Type", "application/json")
                                 .withBody(responseBody)));
 
-        /*
-        DockerImageName localstackImage = DockerImageName.parse("localstack/localstack:0.11.3");
-
-        localstackContainer = new LocalStackContainer(localstackImage)
-                .withServices(SNS);
-        localstackContainer.start();
-
-        AwsCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(
-                AwsBasicCredentials.create(awsAccessKeyId, awsSecretAccessKey)
-        );
-
-        snsClient = SnsClient.builder()
-                .endpointOverride(localstackContainer.getEndpointOverride(SNS))
-                .credentialsProvider(credentialsProvider)
-                .region(Region.of(awsRegion))
-                .build();
-         */
-
         RestAssured.port = serverPort;
         RestAssured.baseURI = "http://localhost";
-    }
-
-    @AfterAll
-    public void tearDown() {
-        // Stop and remove the Localstack container after tests
-        if (localstackContainer != null) {
-            localstackContainer.stop();
-        }
     }
 }

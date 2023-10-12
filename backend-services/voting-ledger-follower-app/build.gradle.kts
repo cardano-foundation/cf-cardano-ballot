7f75d8ef2baf82ecfde36ac9369c51ddeea78c1c
Satya <35016438+satran004@users.noreply.github.com>
2023-10-13 02:07:44 +0800

    fix: Remove spring-boot-starter-data-rest dependency to avoid auto-generation of REST apis for domain models and JPA repositories. (#392)
    Co-authored-by: Mateusz Czeladka <mateusz.szczap@gmail.com>

--- backend-services/voting-ledger-follower-app/build.gradle.kts
+++ backend-services/voting-ledger-follower-app/build.gradle.kts
@@ -36,7 +36,6 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-data-rest")
 	testCompileOnly("org.springframework.boot:spring-boot-starter-test")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-validation")

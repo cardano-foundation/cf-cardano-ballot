7e7a4235aab85f53e7a591efb88f6d4d8ae45d74
Giovanni Gargiulo <giovanni.gargiulo@cardanofoundation.org>
2023-12-18 14:56:56 +0000

    chore: back to 21

--- backend-services/voting-app/build.gradle.kts
+++ backend-services/voting-app/build.gradle.kts
@@ -20,7 +20,7 @@ springBoot {
 
 group = "org.cardano.foundation"
 version = "1.0.0-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 configurations {
     compileOnly {

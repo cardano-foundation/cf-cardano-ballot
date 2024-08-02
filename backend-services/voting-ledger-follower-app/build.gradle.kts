ecf4f9a87afcfdf5c54d578d6eb91c6a20917ea2
Roberto C. Morano <roberto.morano@cardanofoundation.org>
2024-08-02 13:56:57 +0200

    chore(ledger-follower): remove unneeded flyway plugin

--- backend-services/voting-ledger-follower-app/build.gradle.kts
+++ backend-services/voting-ledger-follower-app/build.gradle.kts
@@ -7,9 +7,8 @@ plugins {
 	id("org.springframework.boot") version "3.3.0"
 	id("io.spring.dependency-management") version "1.1.6"
 	id("org.graalvm.buildtools.native") version "0.9.26"
-    id("org.flywaydb.flyway") version "9.22.1"
 	id("cz.habarta.typescript-generator") version "3.2.1263"
-    id("com.github.ben-manes.versions") version "0.48.0"
+  id("com.github.ben-manes.versions") version "0.48.0"
 	jacoco
 }
 

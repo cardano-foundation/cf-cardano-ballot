875f9c7d1bf9e1c834f2d150ad7b303f2d3c368a
J Caso <jcaso@protonmail.com>
2024-09-11 14:51:56 +0200

    fix: test using alias and vite

--- ui/summit-2024/vite.config.ts
+++ ui/summit-2024/vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: "./",
   server: {
     port: 3000
   },
@@ -14,9 +14,11 @@ export default defineConfig({
     },
   },
   build: {
+    outDir: "dist",
+    assetsDir: "assets",
     rollupOptions: {
       output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
+        assetFileNames: `assets/[name].[hash].[ext]`
       }
     }
   }

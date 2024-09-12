315257b50c92c2e037cbf2930184d046ae3bb44c
J Caso <jcaso@protonmail.com>
2024-09-12 15:12:30 +0200

    fix: enum not null missing import

--- backend-services/voting-verification-app/src/main/java/org/cardano/foundation/voting/domain/VoteVerificationRequest.java
+++ backend-services/voting-verification-app/src/main/java/org/cardano/foundation/voting/domain/VoteVerificationRequest.java
@@ -2,6 +2,7 @@ package org.cardano.foundation.voting.domain;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.util.List;

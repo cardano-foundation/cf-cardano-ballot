129b77833f4016f81fce28bb672d695adc87297a
Florian Schumann <florian.schumann@cardanofoundation.org>
2025-07-31 16:42:07 +0200

    chore(docker): build summit 2025 ui

---
+++ ui/summit-2025/docker-assets/bin/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+ENV_FILE_PATH=/usr/share/nginx/html/envfile.js
+#export VITE_VERSION=$(grep -m 1 "version" package.json | awk '{print $2}' | sed 's/[",]//g')
+
+echo "window._env_ = {" > $ENV_FILE_PATH
+for var in $(env | grep ^VITE_); do
+  echo "  \"${var%%=*}\": \"${var#*=}\"," >> $ENV_FILE_PATH
+done
+echo "};" >> $ENV_FILE_PATH
+
+exec nginx -g "daemon off;" $@
